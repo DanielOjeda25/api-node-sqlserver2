827b08b250b452ca05c33bcf7c9b53a102e8cb72
Daniel Ojea <ojedandanielalejandro333@gmail.com>
2023-02-02 10:58:13 -0300

    queries para el CRUD

--- src/database/querys.js
+++ src/database/querys.js
@@ -3,5 +3,6 @@ export const queries = {
   AddNewProduct: 'INSERT INTO webstore.dbo.Products (Name, Description, Quantity) VALUES (@Name, @Description, @Quantity);',
   getProductByID: 'SELECT * FROM  webstore.dbo.Products WHERE id = @id;',
   deleteProductById: 'DELETE FROM webstore.dbo.Products WHERE id = @id;',
-  getTotalProduct: 'SELECT COUNT(*) FROM webstore.dbo.Products;'
+  getTotalProduct: 'SELECT COUNT(*) AS TOTAL FROM webstore.dbo.Products;',
+  updateproductById: 'UPDATE webstore.dbo.Products SET Name = @Name, Description = @Description, Quantity = @Quantity WHERE id = @id;'
 }
